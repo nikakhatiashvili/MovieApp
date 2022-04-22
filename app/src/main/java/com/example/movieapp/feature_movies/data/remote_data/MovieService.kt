f442f171268203906e91424cfb11b9821c8e5008
nikakhatiashvili <75098057+nikakhatiashvili@users.noreply.github.com>
2022-04-22 14:20:59 +0400

    adding popular movies

--- app/src/main/java/com/example/movieapp/feature_movies/data/remote_data/MovieService.kt
+++ app/src/main/java/com/example/movieapp/feature_movies/data/remote_data/MovieService.kt
@@ -1,12 +1,16 @@
 package com.example.movieapp.feature_movies.data.remote_data
 
 
+import com.example.movieapp.feature_movies.domain.model.popular.Popular
 import com.example.movieapp.feature_movies.domain.model.top_rated.TopRated
-import com.example.movieapp.feature_movies.domain.utils.Constants.GET_NEWS
+import com.example.movieapp.feature_movies.domain.utils.Constants.GET_POPULAR_MOVIES
+import com.example.movieapp.feature_movies.domain.utils.Constants.GET_TOP_MOVIES
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface MovieService {
-    @GET(GET_NEWS)
+    @GET(GET_TOP_MOVIES)
     suspend fun getAllTopRatedMovies(): Response<TopRated>
+    @GET(GET_POPULAR_MOVIES)
+    suspend fun getPopular(): Response<Popular>
 }
--- app/src/main/java/com/example/movieapp/feature_movies/domain/model/top_rated/TopRated.kt
+++ app/src/main/java/com/example/movieapp/feature_movies/domain/model/top_rated/TopRated.kt
@@ -3,4 +3,6 @@ package com.example.movieapp.feature_movies.domain.model.top_rated
 data class TopRated(
     val page: Double?,
     val results: List<TopRatedMovies>?,
+    val total_pages: Int?,
+    val total_results: Int?
 )
