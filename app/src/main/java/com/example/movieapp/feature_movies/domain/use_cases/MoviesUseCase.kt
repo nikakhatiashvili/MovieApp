559af1a6e05f50f04da77fdc2c6c5458c0bcc9e1
nikakhatiashvili <75098057+nikakhatiashvili@users.noreply.github.com>
2022-04-22 15:35:02 +0400

    added upcoming movies request

--- app/src/main/java/com/example/movieapp/feature_movies/domain/use_cases/MoviesUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.example.movieapp.feature_movies.domain.use_cases
-
-data class MoviesUseCase(
-    val topRatedUseCase: TopRatedUseCase,
-    val popularUseCase: PopularUseCase,
-)
--- app/src/main/java/com/example/movieapp/feature_movies/domain/use_cases/top_rated/TopRatedUseCase.kt
+++ app/src/main/java/com/example/movieapp/feature_movies/domain/use_cases/top_rated/TopRatedUseCase.kt
@@ -1,6 +1,5 @@
-package com.example.movieapp.feature_movies.domain.use_cases
+package com.example.movieapp.feature_movies.domain.use_cases.top_rated
 
-import com.example.movieapp.feature_movies.domain.model.popular.Popular
 import com.example.movieapp.feature_movies.domain.model.top_rated.TopRated
 import com.example.movieapp.feature_movies.domain.repository.MoviesRepository
 import com.example.movieapp.feature_movies.domain.utils.Resource
@@ -8,11 +7,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class PopularUseCase @Inject constructor(private val popularRepository: MoviesRepository) {
-    operator fun invoke(): Flow<Resource<Popular>> = flow {
+class TopRatedUseCase @Inject constructor(private val movieRepository: MoviesRepository) {
+    operator fun invoke(): Flow<Resource<TopRated>> = flow {
         try {
             emit(Resource.Loading())
-            emit(popularRepository.popularMovies())
+            emit(movieRepository.topRatedMovies())
         } catch (e: Exception) {
             emit(Resource.Error("Exception"))
         }
